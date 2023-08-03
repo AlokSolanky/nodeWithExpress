0787c1d27e5f4c1607186f4eb34aed19779e82ed
AlokSolanky <aloksolanky1@gmail.com>
2023-08-03 23:37:09 +0530

    2 middleware attached

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const express = require('express');
+
+const app = express();
+app.use((req,res,next)=>
+{
+    console.log(`<h1>Hello world</h1>`);
+    next();
+})
+app.use((req,res,next)=>
+{
+    res.send(`<h2> this is second middleware</h2>`);
+})
+
+app.listen(4000);
