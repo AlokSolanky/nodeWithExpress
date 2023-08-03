dcc8b91830be9caa8b1a8e3b49225769e4d8265d
AlokSolanky <aloksolanky1@gmail.com>
2023-08-04 02:44:17 +0530

    messaging app done

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoute = require('./routes/admin');
-const shopRoute = require('./routes/shop');
+const fs = require('fs');
+const { LocalStorage } = require("node-localstorage");
+
+
+const localStorage = new LocalStorage("./local-storage"); 
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/admin',adminRoute);
-app.use(shopRoute);
-app.use((req,res)=>
+app.get("/login",(req,res,next)=>
 {
-    res.status(404).send(`404 ---- Page Not Found`);
+    res.send(
+      `<form method="POST" action="add-user"><input name = "username" type"text"><button type="submit">Login</button></form>`
+    );
+})
+app.post("/add-user",(req,res,next)=>
+{
+    const name = req.body.username;
+    localStorage.setItem("user",name);
+    res.redirect("/");
+})
+app.get("/",(req,res,next)=>
+{
+    const data = fs.readFileSync("messages.txt");
+    res.send(
+      `<p>${data} <br> <form method="POST" action="add-msg"><input name = "message" type"text"><button type="submit">Send</button></form>`
+    );
+})
+app.post("/add-msg",(req,res,next)=>
+{
+    const user = localStorage.getItem("user");
+    const msg = req.body.message;
+
+    fs.appendFileSync("messages.txt", `${user} : ${msg} \n`);
+    res.redirect("/");
 })
 app.listen(4000);
